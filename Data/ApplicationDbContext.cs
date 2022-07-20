8e24e49b68d3dd5d26aba65296bbab78826c6e27
RosenStefanov <95367525+RosenStefanov@users.noreply.github.com>
2022-07-20 21:56:25 +0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OmegaParts.Models;
+
+namespace OmegaParts.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    public DbSet<Clutch_Model> Clutches { get; set; }
+    
+    public DbSet<Order_Model> Orders { get; set; }
+    
+    public DbSet<Head_Gasket_Model> Head_Gasket { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+        
+    }
+}
---
+++ Migrations/20220607192336_Clutch_Model.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmegaParts.Migrations
+{
+    /// <inheritdoc />
+    public partial class Clutch_Model : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clutches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Make_and_model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Diam = table.Column<float>(type: "real", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clutches", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clutches");
+        }
+    }
+}
